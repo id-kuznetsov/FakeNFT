749111e505ad4b84935668eb33b049392a7c5676
Alekse1Frolov <aleksei.frolov@customertimes.com>
2025-02-14 02:12:45 +0100

    Удалил Main.storyboard, настроил TabBar

---
+++ FakeNFT/Scenes /Statistics/StatisticsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StatisticsViewController.swift
+//  FakeNFT
+//
+//  Created by Aleksei Frolov on 13.02.2025.
+//
+
+import UIKit
+
+final class StatisticsViewController: UIViewController {
+    
+    private let servicesAssembly: ServicesAssembly
+
+    init(servicesAssembly: ServicesAssembly) {
+        self.servicesAssembly = servicesAssembly
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,25 +1,59 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
-    var servicesAssembly: ServicesAssembly!
-
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
-        tag: 0
-    )
-
+    
+    private let servicesAssembly: ServicesAssembly
+    
+    init(servicesAssembly: ServicesAssembly) {
+        self.servicesAssembly = servicesAssembly
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let catalogController = TestCatalogViewController(
-            servicesAssembly: servicesAssembly
-        )
-        catalogController.tabBarItem = catalogTabBarItem
-
-        viewControllers = [catalogController]
-
+        
         view.backgroundColor = .systemBackground
+        configure()
+    }
+    
+    private func configure() {
+        let profileVC = UINavigationController(rootViewController: ProfileViewController(servicesAssembly: servicesAssembly))
+        profileVC.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.profile", comment: ""),
+            image: UIImage(systemName: "person.circle"),
+            selectedImage: UIImage(systemName: "person.circle.fill")
+        )
+        
+        let catalogVC = UINavigationController(rootViewController: CatalogViewController(servicesAssembly: servicesAssembly))
+        catalogVC.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.catalog", comment: ""),
+            image: UIImage(systemName: "square.stack.3d.up"),
+            selectedImage: UIImage(systemName: "square.stack.3d.up.fill")
+        )
+        
+        let cartVC = UINavigationController(rootViewController: CartViewController(servicesAssembly: servicesAssembly))
+        cartVC.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.cart", comment: ""),
+            image: UIImage(systemName: "bag"),
+            selectedImage: UIImage(systemName: "bag.fill")
+        )
+        
+        let statisticsVC = UINavigationController(rootViewController: StatisticsViewController(servicesAssembly: servicesAssembly))
+        statisticsVC.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.statistics", comment: ""),
+            image: UIImage(systemName: "chart.bar"),
+            selectedImage: UIImage(systemName: "chart.bar.fill")
+        )
+        
+        setViewControllers([profileVC, catalogVC, cartVC, statisticsVC], animated: false)
+        self.selectedIndex = 1
+        
+        tabBar.tintColor = .blue
+        tabBar.unselectedItemTintColor = .black
+        tabBar.backgroundColor = .systemBackground
     }
 }
