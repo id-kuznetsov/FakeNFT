13ab92059adbc3aebcd2abc3c2087f78e4c18077
Alekse1Frolov <aleksei.frolov@customertimes.com>
2025-02-16 23:26:53 +0100

    Added custom navigation bar with filter button

--- FakeNFT/Scenes /Statistics/StatisticsViewController.swift
+++ FakeNFT/Scenes /Statistics/StatisticsViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 final class StatisticsViewController: UIViewController {
     
+    // MARK: - Dependencies
     private let servicesAssembly: ServicesAssembly
-
+    
+    // MARK: - UI Elements
+    private lazy var customNavBar: UINavigationBar = {
+        let navBar = UINavigationBar()
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        navBar.barTintColor = .ypWhite
+        navBar.shadowImage = UIImage()
+        return navBar
+    }()
+    
+    private lazy var filterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "ic.filter"), for: .normal)
+        button.tintColor = .ypBlack
+        button.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Init
     init(servicesAssembly: ServicesAssembly) {
         self.servicesAssembly = servicesAssembly
         super.init(nibName: nil, bundle: nil)
@@ -20,9 +39,44 @@ final class StatisticsViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        setupUI()
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(customNavBar)
+        
+        NSLayoutConstraint.activate([
+            customNavBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            customNavBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            customNavBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        configureCustomNavBar()
+    }
+    
+    private func configureCustomNavBar() {
+        let navItem = UINavigationItem()
+        let filterBarButton = UIBarButtonItem(customView: filterButton)
+        navItem.rightBarButtonItem = filterBarButton
+        
+        customNavBar.setItems([navItem], animated: false)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 19.5
+        navItem.rightBarButtonItems = [spacer, filterBarButton]
+    }
+    
+    // MARK: - Actions
+    @objc private func filterButtonTapped() {
+        // TO DO: Add a call to AlertPresenter with sorting options by name and by rating.
     }
 }
