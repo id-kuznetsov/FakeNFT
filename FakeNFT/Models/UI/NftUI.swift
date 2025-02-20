03b15aeec74b923984c1f3f80a59c104fbd61c4f
Nikolai Eremenko <nikolai.dev@icloud.com>
2025-02-20 13:13:51 +0400

    feat: add nfts service

--- FakeNFT/Models/UI/NftUI.swift
+++ FakeNFT/Models/UI/NftUI.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 struct NftUI: Decodable {
+    let createdAt: Date
     let name: String
-    let cover: URL?
+    let images: [URL?]
+    let rating: Int
+    let description: String
+    let price: Double
+    let author: URL?
     let id: String
 }
