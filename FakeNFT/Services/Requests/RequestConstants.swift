794d70b06781a4d6460c14ee7c21fa0fb0d0dfe0
Nikolai Eremenko <nikolai.dev@icloud.com>
2025-03-04 09:54:41 +0400

    build: delete ProgressHUD

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,6 +1,5 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
     #warning("Insert your token here")
-    #warning("Insert your token here")
     static let token = ""
 }
