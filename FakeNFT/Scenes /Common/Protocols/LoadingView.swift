bc908ccb768c96340dbdcfde546681d222596980
Nikolai Eremenko <nikolai.dev@icloud.com>
2025-03-05 20:35:51 +0400

    refactor(LoadingView): add custom loading view

--- FakeNFT/Scenes /Common/Protocols/LoadingView.swift
+++ FakeNFT/Scenes /Common/Protocols/LoadingView.swift
@@ -1,18 +1,42 @@
-import ProgressHUD
 import UIKit
 
 protocol LoadingView {
-    var activityIndicator: UIActivityIndicatorView { get }
     func showLoading()
     func hideLoading()
 }
 
-extension LoadingView {
+extension LoadingView where Self: UIViewController {
+    var activityIndicator: CustomLoadingView {
+        let tag = 9999
+        if let existingIndicator = view.viewWithTag(tag) as? CustomLoadingView {
+            return existingIndicator
+        }
+
+        let customLoadingView = CustomLoadingView()
+        customLoadingView.tag = tag
+        customLoadingView.layer.cornerRadius = 10
+        customLoadingView.clipsToBounds = true
+        customLoadingView.backgroundColor = .ypWhite
+        customLoadingView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(customLoadingView)
+
+        NSLayoutConstraint.activate([
+            customLoadingView.widthAnchor.constraint(equalToConstant: 80),
+            customLoadingView.heightAnchor.constraint(equalToConstant: 80),
+            customLoadingView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            customLoadingView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+
+        return customLoadingView
+    }
+
     func showLoading() {
         activityIndicator.startAnimating()
     }
 
     func hideLoading() {
         activityIndicator.stopAnimating()
+        activityIndicator.removeFromSuperview()
     }
 }
+
